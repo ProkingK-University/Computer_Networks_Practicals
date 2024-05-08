b2bb096638891d229f6e3a7ff5ed9a3145f433d7
Ashley Kapaso <ashleykapaso@gmail.com>
2024-05-08 13:19:44 +0200

    practical 7 setup

---
+++ Practical_7/main.py
@@ -0,0 +1,36 @@
+import socket
+
+get_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+send_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+get_server.connect(('localhost', 110))
+get_server.recv(4096)
+
+get_server.send(b'USER prokingk\n')
+get_server.recv(4096)
+get_server.send(b'PASS <password>\n')
+get_server.recv(4096)
+
+get_server.send(b'LIST\n')
+get_server.recv(4096)
+get_server.send(b'RETR 1\n')
+
+email = get_server.recv(4096)
+print(email.decode())
+
+get_server.close()
+
+send_server.connect(('localhost', 25))
+send_server.recv(4096)
+
+send_server.send(b'HELO localhost\n')
+send_server.recv(4096)
+send_server.send(b'MAIL FROM: <user@localhost>\n')
+send_server.recv(4096)
+send_server.send(b'RCPT TO: <prokingk@localhost>\n')
+send_server.recv(4096)
+send_server.send(b'DATA\n')
+send_server.recv(4096)
+send_server.send(b'Subject: Warning\nTest warning\n.\n')
+send_server.recv(4096)
+send_server.send(b'QUIT\n')
