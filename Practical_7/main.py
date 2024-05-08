1f572ee88d1012b5a4910e4601372ef0eb10fdce
Ashley Kapaso <ashleykapaso@gmail.com>
2024-05-08 21:50:36 +0200

    add bcc functionality

--- Practical_7/main.py
+++ Practical_7/main.py
@@ -1,36 +1,64 @@
+import re
 import socket
 
-get_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-send_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pop3_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-get_server.connect(('localhost', 110))
-get_server.recv(4096)
+pop3_server.connect(('localhost', 110))
+pop3_server.recv(4096)
+pop3_server.send(b'USER prokingk\n')
+pop3_server.recv(4096)
+pop3_server.send(b'PASS <password>\n')
+pop3_server.recv(4096)
+pop3_server.send(b'LIST\n')
 
-get_server.send(b'USER prokingk\n')
-get_server.recv(4096)
-get_server.send(b'PASS <password>\n')
-get_server.recv(4096)
+response = pop3_server.recv(4096).decode()
+email_count = len(response.split('\n')) - 2
 
-get_server.send(b'LIST\n')
-get_server.recv(4096)
-get_server.send(b'RETR 1\n')
+for i in range(1, email_count + 1):
+    email_data = b''
+    pop3_server.send(f'RETR {i}\n'.encode())
 
-email = get_server.recv(4096)
-print(email.decode())
+    while True:
+        data = pop3_server.recv(4096)
 
-get_server.close()
+        if data.endswith(b'\r\n.\r\n'):
+            email_data += data[:-5]
+            break
 
-send_server.connect(('localhost', 25))
-send_server.recv(4096)
+        email_data += data
 
-send_server.send(b'HELO localhost\n')
-send_server.recv(4096)
-send_server.send(b'MAIL FROM: <user@localhost>\n')
-send_server.recv(4096)
-send_server.send(b'RCPT TO: <prokingk@localhost>\n')
-send_server.recv(4096)
-send_server.send(b'DATA\n')
-send_server.recv(4096)
-send_server.send(b'Subject: Warning\nTest warning\n.\n')
-send_server.recv(4096)
-send_server.send(b'QUIT\n')
+    email_str = email_data.decode()
+    
+    subject_match = re.search(r'Subject:\s*(.+)', email_str, re.IGNORECASE)
+    subject = subject_match.group(1).strip() if subject_match else 'No Subject'
+
+    print(f'Email {i} Subject: {subject}')
+    
+    bcc_match = re.search(r'Bcc:\s*(.+)', email_str, re.IGNORECASE)
+
+    if bcc_match:
+        bcc_recipients = bcc_match.group(1).split(',')
+
+        if 'prokingk@localhost' in [r.strip() for r in bcc_recipients]:
+            smtp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            smtp_server.connect(('localhost', 25))
+            smtp_server.recv(4096)
+            smtp_server.send(b'HELO localhost\n')
+            smtp_server.recv(4096)
+            smtp_server.send(b'MAIL FROM: <user@localhost>\n')
+            smtp_server.recv(4096)
+            smtp_server.send(b'RCPT TO: <prokingk@localhost>\n')
+            smtp_server.recv(4096)
+            smtp_server.send(b'DATA\n')
+            smtp_server.recv(4096)
+
+            warning_message = f'Subject: [BCC Warning] {subject}\n\nYou received a BCC email with the subject: {subject}\n.\n'
+            smtp_server.send(warning_message.encode())
+            smtp_server.recv(4096)
+
+            smtp_server.send(b'QUIT\n')
+
+            smtp_server.close()
+
+pop3_server.close()
